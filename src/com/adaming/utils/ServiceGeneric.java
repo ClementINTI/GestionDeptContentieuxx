bd7ec168221d92880938f454d034141ea32261d2
Gerda1234 <ranokegerda@gmail.com>
2019-05-14 12:05:38 +0200

    Les controllers

--- src/com/adaming/utils/ServiceGeneric.java
+++ src/com/adaming/utils/ServiceGeneric.java
@@ -2,16 +2,18 @@ package com.adaming.utils;
 
 import java.util.List;
 
+import com.adaming.entities.Affaire;
+
 public interface ServiceGeneric  <T> {
 
-	public void create(T obj);
+	public Affaire create(T obj);
 	
 	public void update(T obj);
 	
-	public void delete (T obj);
+	public void delete (Long id);
 	
 	public T getById(Class<T> obj, Long id);
 	
-	public List<T> find(Class<T> obj);
+	public List<T> find(Long id);
 
 }
